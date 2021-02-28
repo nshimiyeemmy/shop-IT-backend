c7563fdec7348aa885b372c7d8326b59ddb960b7
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-02-28 16:45:09 +0200

    Updated with Save Product in the database functionality

--- back_end/controllers/productController.js
+++ back_end/controllers/productController.js
@@ -1,5 +1,16 @@
 // To manage all product related logics or functions, controllers functions
 
+const products = require('../models/products');
+
+exports.newProducts = async (req,res,next)=>{
+    const product = await products.create(req.body);
+    res.status(201).json({
+        success: true,
+        message:'Product was successfully saved into the Database.',
+        product
+    })
+}
+
 exports.getProducts = (req,res,next)=>{
     res.status(200).json({
         success: true,
--- back_end/routes/products.js
+++ back_end/routes/products.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { getProducts } = require('../controllers/productController');
+const { newProducts, getProducts } = require('../controllers/productController');
 
-
-router.route('/products').get(getProducts); 
+router.route('/products').get(getProducts);
+router.route('/products/new').post(newProducts);
 
 
 module.exports = router;
