855b5c1f1bb6eccd55ddeb8263ebc7c0d73ee778
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-03-02 05:30:31 +0200

    Updated with product seeder method

---
+++ back_end/utils/seeder.js
@@ -0,0 +1,27 @@
+const Product = require('../models/products');
+const dotenv = require('dotenv');
+const connectToDatabase = require('../config/database');
+const products = require('../data/products.json');
+
+//setting up the dotenv file
+dotenv.config({path: 'back_end/config/config.env'});
+//connecting to the database
+connectToDatabase();
+
+const seedProducts= async ()=>{
+    try {
+
+        await Product.deleteMany();
+        console.log('All Products deleted Successfully');
+
+        await Product.insertMany(products)
+        console.log("All Products Inserted Successfully");
+        process.exit();
+        
+    } catch (error) {
+        console.log(error.message);
+        process.exit();
+    }y
+}
+seedProducts();
+
