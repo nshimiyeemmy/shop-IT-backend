48471f959c3d8cca292d150f1f3e69b3befa9176
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-03-02 18:34:44 +0200

    updated with global error handle

---
+++ back_end/middlewares/errors.js
@@ -0,0 +1,13 @@
+const ErrorHandler = require('../utils/errorHandler');
+
+module.exports = (err,req,res,next)=>{
+    err.statusCode = err.statusCode || 500;
+    err.message = err.message || 'Internal Server error';
+
+    res.status(err.statusCode).json({
+        success:false,
+        error:err
+
+    })
+
+}
