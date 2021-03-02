7235cf448a99e035985ed5adf749756104565c76
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-03-02 18:33:12 +0200

    Updated with implementing the error handling middleware

--- back_end/app.js
+++ back_end/app.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const app = express();
+const errorMiddleware = require('./middlewares/errors');
 
 app.use(express.json());
-
 //Importing all the routes
 const products = require('./routes/products');
 
 // doing routes
 app.use('/api/v1', products);
 
-
+//Implementing the middleware to handle errors
+app.use(errorMiddleware);
 
 module.exports = app
