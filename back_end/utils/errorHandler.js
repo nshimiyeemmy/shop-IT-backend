4e5a2cddd2ec9c91acfe49f0c7394e318559493d
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-03-02 18:35:15 +0200

    Upfdated with Error handler class

--- back_end/utils/errorHandler.js
+++ back_end/utils/errorHandler.js
@@ -4,7 +4,6 @@ class ErrorHandler extends Error{
 constructor(message,statusCode){
     super(message);
     this.statusCode = statusCode;
-
     Error.captureStackTrace(this, this.constructor);
 
 }
