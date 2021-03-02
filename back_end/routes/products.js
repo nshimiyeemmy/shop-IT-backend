d128b8d9dec7cb064d72c562978aee3226f2808a
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-03-02 17:16:05 +0200

    updated with delete product route

--- back_end/routes/products.js
+++ back_end/routes/products.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const {updateProduct,getSingleProduct,newProducts, getProducts } = require('../controllers/productController');
+const {getProducts,getSingleProduct,newProducts,updateProduct,deleteProduct } = require('../controllers/productController');
 
 router.route('/products').get(getProducts);
-router.route('/admin/products/new').post(newProducts);
 router.route('/products/:id').get(getSingleProduct);
-router.route('/admin/products/:id').put(updateProduct);
+router.route('/admin/products/new').post(newProducts);
+router.route('/admin/products/:id').put(updateProduct)
+                                   .delete(deleteProduct);
+
+
 
 
 
