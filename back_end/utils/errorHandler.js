9f80c07b5ce99db6f781901abafbe23cbfe2731e
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-03-03 08:26:58 +0200

    updatedwith proper implementation of handling dev & prod errors

--- back_end/utils/errorHandler.js
+++ back_end/utils/errorHandler.js
@@ -3,10 +3,10 @@
 class ErrorHandler extends Error{
 constructor(message,statusCode){
     super(message);
-    this.statusCode = statusCode;
+    this.statusCode = statusCode
+
     Error.captureStackTrace(this, this.constructor);
 
 }
 }
-
-module.exports = ErrorHandler
+module.exports = ErrorHandler;
