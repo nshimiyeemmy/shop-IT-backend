6b6630abb93a1a339bd47334f332a7bef4857e85
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-03-03 08:25:53 +0200

    error fixed

--- back_end/middlewares/errors.js
+++ back_end/middlewares/errors.js
@@ -1,24 +1,27 @@
 const ErrorHandler = require('../utils/errorHandler');
 
 module.exports = (err,req,res,next)=>{
+    const env = process.env.NODE_ENV.trim()
     err.statusCode = err.statusCode || 500;
+
+   if(env === "DEVELOPMENT"){
+    res.status(err.statusCode).json({
+        success:false,
+        error:err,
+        errMessage:err.message,
+        stack:err.stack
+    })
+   }
+
+   if (env === "PRODUCTION"){
+       let error = {...err}
+       error.message  = err.message
+    res.status(error.statusCode).json({
+        success:false,
+        message: error.message || 'Internal Server Error'
+    }) 
+   }
     
-    if(process.env.NODE_DEV == 'DEVELOPMENT'){
-        res.status(err.statusCode).json({
-            success:false,
-            error:err,
-            errMessage:err.message,
-            stack:err.stack
-        })
-    }
-     if(process.env.NODE_DEV == 'PRODUCTION'){
-        let error = {...err}
-        error.message = err.message
 
-        res.status(error.statusCode).json({
-            success:false,
-            message:error.message || 'Internal Server Error' 
-        })
-
-    }
+   
 }
