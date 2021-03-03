16ec746bb21cbdcbdc560ad922c51f050db30378
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-03-03 16:31:55 +0200

    Updated with adding functionality of pagination of products on our pages

--- back_end/utils/apiFeatures.js
+++ back_end/utils/apiFeatures.js
@@ -33,5 +33,16 @@ filter(){
     return this;
 }
 
+//Implementing Pagination method
+
+pagination(resPerPage){
+    const currentPage = Number(this.queryStr.page) || 1;
+    const skip = resPerPage * (currentPage - 1);
+
+    this.query = this.query.limit(resPerPage).skip(skip);
+    return this;
+
+}
+
 }
 module.exports = APIFeatures
