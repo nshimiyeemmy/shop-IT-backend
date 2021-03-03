b1d43d658a62bdd80716d41a442c511705f99f19
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-03-03 15:11:34 +0200

    Updated with filtering products by categories

--- back_end/utils/apiFeatures.js
+++ back_end/utils/apiFeatures.js
@@ -16,7 +16,18 @@ class APIFeatures{
     this.query = this.query.find({...keyword})
     return this;
 }
+
+// Building the filtering functionality on the routes
+filter(){
+    const queryCopy = {...this.queryStr};
+
+    //removing fields from the query
+    const removeFields = ['keyword', 'limit', 'page']
+    removeFields.forEach(el => delete queryCopy[el]);
+
+    this.query = this.query.find(queryCopy)
+    return this;
 }
 
-
+}
 module.exports = APIFeatures
