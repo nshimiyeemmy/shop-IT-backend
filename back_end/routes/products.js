47f68117580cc25a0c6496d1b8189d3406b4f67c
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-03-03 08:26:25 +0200

    updated with handle errors globally

--- back_end/routes/products.js
+++ back_end/routes/products.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const {getProducts,getSingleProduct,newProducts,updateProduct,deleteProduct } = require('../controllers/productController');
+
+const { getSingleProduct,newProducts, getProducts,updateProduct,deleteProduct } = require('../controllers/productController');
 
 router.route('/products').get(getProducts);
-router.route('/products/:id').get(getSingleProduct);
 router.route('/admin/products/new').post(newProducts);
+router.route('/products/:id').get(getSingleProduct);
 router.route('/admin/products/:id').put(updateProduct)
-                                   .delete(deleteProduct);
-
-
-
-
+                                    .delete(deleteProduct)
 
 module.exports = router;
