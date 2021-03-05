f8ebf90b12481fe753ee38405707c474982dfd8e
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-03-06 01:22:25 +0200

    update with authorize User Roles and Permissions method

--- back_end/middlewares/authenticate.js
+++ back_end/middlewares/authenticate.js
@@ -15,3 +15,15 @@ exports.isUserAuthenticated = catchAsyncErrors(async (req,res,next)=>{
         req.user = await User.findById(decoded.id);
         next()
 })
+
+
+//Handling the users roles
+
+exports.authorizeRole = (...roles)=>{
+    return (req, res, next) =>{
+        if(!roles.includes(req.user.role)){
+          return next(new ErrorHandler(`Role (${req.user.role}) is not allowed to access this resource`,403))
+        }
+        next()
+    }
+}
