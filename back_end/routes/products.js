74852b3ebab42ef07fc1662cf881c09252a7692c
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-03-06 00:36:49 +0200

    updated with the secure routes

--- back_end/routes/products.js
+++ back_end/routes/products.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 
 const { getSingleProduct,newProducts, getProducts,updateProduct,deleteProduct } = require('../controllers/productController');
-
-router.route('/products').get(getProducts);
+const {isUserAuthenticated} = require('../middlewares/authenticate');
+router.route('/products').get(isUserAuthenticated,getProducts);
 router.route('/admin/products/new').post(newProducts);
 router.route('/products/:id').get(getSingleProduct);
 router.route('/admin/products/:id').put(updateProduct)
