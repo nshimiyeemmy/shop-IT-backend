3e31f9634326e0db5f07e576c11db9c1812c00d3
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-03-05 23:08:57 +0200

    updated with implementing the generate jwt method

--- back_end/models/users.js
+++ back_end/models/users.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
 
@@ -56,6 +57,13 @@ userSchema.pre('save', async function(next){
     this.password = await bcrypt.hash(this.password, 10)
 
 })
+
+//returning jsonwebtoken when user registers or logins in
+userSchema.methods.getJwtToken = function(){
+return jwt.sign({id:this._id},process.env.JWT_SECRET,{
+    expiresIn:process.env.JWT_EXPIRES_TIME
+});
+}
 
 
 module.exports = mongoose.model('Users', userSchema);
