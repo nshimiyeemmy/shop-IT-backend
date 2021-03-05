602836c79447cb82e5ff0f59b31d1a6df9fc9ff4
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-03-06 00:09:48 +0200

    implements the send token into cookie functionality

---
+++ back_end/utils/JWT.js
@@ -0,0 +1,19 @@
+//create, send and save the JWT token in the cookie
+const sendToken = (user, statusCode,res)=>{
+    //create JWT token
+    const token = user.getJwtToken();
+    //create Options for the cookie
+    const options = {
+        expires: new Date(
+            Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
+        ),
+        httpOnly:true
+    }
+
+    res.status(statusCode).cookie('token',token,options).json({
+        success:true,
+        token:token,
+        User:user
+    })
+}
+module.exports = sendToken
