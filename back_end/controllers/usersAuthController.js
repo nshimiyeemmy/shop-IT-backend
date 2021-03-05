19ca11d153c778a8a24f4625e52a1ab3c931ee1c
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-03-06 00:56:13 +0200

    Updated with the logout functionality

--- back_end/controllers/usersAuthController.js
+++ back_end/controllers/usersAuthController.js
@@ -48,3 +48,15 @@ exports.loginUser = catchAsyncErrors(async (req,res,next)=>{
      }
      sendToken(user ,200 ,res);
 })
+
+//logout User  => /api/v1/logout
+exports.logoutUser = catchAsyncErrors(async (req,res,next)=>{
+    res.cookie('token',null,{
+        expires:new Date(Date.now()),
+        httpOnly:true
+    })
+    res.status(200).json({
+        success:true,
+        message:'Logged Out Successfully'
+    })
+})
