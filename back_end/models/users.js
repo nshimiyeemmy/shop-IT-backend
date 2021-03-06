365734e4bc09742d56d775ff69ebea7ec47ab4ed
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-03-06 08:20:20 +0200

    updated with the reset password token generated and set to it's expirer time

--- back_end/models/users.js
+++ back_end/models/users.js
@@ -2,13 +2,16 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+
 
 const userSchema = new mongoose.Schema({
 
     firstname:{
         type:String,
         required:[true,'Please enter your first name'],
-        maxlength:[30, 'Your first name can not exceed 30 characters']
+        maxlength:[30, 'Your first name can not exceed 30 characters'],
+        validate:[validator.isAlpha,'Please enter only String values']
     },
     lastname:{
         type:String,
@@ -68,6 +71,19 @@ userSchema.methods.getJwtToken = function(){
 return jwt.sign({id:this._id},process.env.JWT_SECRET,{
     expiresIn:process.env.JWT_EXPIRES_TIME
 });
+}
+
+//Generating the password reset token
+userSchema.methods.getResetPasswordToken = function(){
+    //generate the token
+    const resetToken = crypto.randomBytes(20).toString('hex');
+    //hash & send to getResetPasswordToken, this b'se it's not a good idea to send the unencrpted token
+this.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+     //set the reset token expires time
+     this.resetPasswordExpire = Date.now() + 30 * 60 * 1000
+
+     return resetToken;
+
 }
 
 
