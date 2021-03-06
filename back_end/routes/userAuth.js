28f1faa15f082211714e88b003818f205e02eb82
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-03-06 09:37:20 +0200

    updated with the forgot password route

--- back_end/routes/userAuth.js
+++ back_end/routes/userAuth.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 
-const {registerUser,loginUser,logoutUser} = require('../controllers/usersAuthController');
+const {registerUser,loginUser,forgotPassword,logoutUser} = require('../controllers/usersAuthController');
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
+router.route('/password/forgot').post(forgotPassword);
+
 router.route('/logout').get(logoutUser);
 
 
