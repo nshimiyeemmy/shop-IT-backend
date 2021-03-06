40a89a86b599d26f8454f818e738fb8484043c6c
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-03-06 10:10:33 +0200

    updated with the reset password route working perfectly fine

--- back_end/routes/userAuth.js
+++ back_end/routes/userAuth.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 
-const {registerUser,loginUser,forgotPassword,logoutUser} = require('../controllers/usersAuthController');
+const {registerUser,loginUser,forgotPassword,resetPassword,logoutUser} = require('../controllers/usersAuthController');
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
 router.route('/password/forgot').post(forgotPassword);
+router.route('/password/reset/:token').put(resetPassword);
 
 router.route('/logout').get(logoutUser);
 
