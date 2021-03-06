58a6f9ac95cf406f6319039c69e6bc0d23176d40
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-03-06 12:37:28 +0200

    updated with the update password route

--- back_end/routes/userAuth.js
+++ back_end/routes/userAuth.js
@@ -2,14 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 
-const {registerUser,loginUser,forgotPassword,resetPassword,getUserProfile,logoutUser} = require('../controllers/usersAuthController');
+const {registerUser,loginUser,forgotPassword,resetPassword,getUserProfile,updatePassword,logoutUser} = require('../controllers/usersAuthController');
 const {isUserAuthenticated} = require('../middlewares/authenticate');
 
-router.route('/me').get(isUserAuthenticated,getUserProfile);
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
 router.route('/password/forgot').post(forgotPassword);
 router.route('/password/reset/:token').put(resetPassword);
+router.route('/me').get(isUserAuthenticated,getUserProfile);
+router.route('/password/update').put(isUserAuthenticated,updatePassword);
+
 
 router.route('/logout').get(logoutUser);
 
