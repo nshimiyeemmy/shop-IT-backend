d225af809c5a29def101ec266b06ad34b06aa176
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-03-06 11:02:41 +0200

    Updated with get  logged in user profile route

--- back_end/routes/userAuth.js
+++ back_end/routes/userAuth.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 
-const {registerUser,loginUser,forgotPassword,resetPassword,logoutUser} = require('../controllers/usersAuthController');
+const {registerUser,loginUser,forgotPassword,resetPassword,getUserProfile,logoutUser} = require('../controllers/usersAuthController');
+const {isUserAuthenticated} = require('../middlewares/authenticate');
 
+router.route('/me').get(isUserAuthenticated,getUserProfile);
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
 router.route('/password/forgot').post(forgotPassword);
