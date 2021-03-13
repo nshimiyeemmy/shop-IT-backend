0f9f06908f3c71c809893e45056f09ef3d37a59f
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-03-13 14:07:40 +0200

    updated with get single user details - Admin

--- back_end/controllers/usersAuthController.js
+++ back_end/controllers/usersAuthController.js
@@ -181,4 +181,18 @@ exports.getAllUsers = catchAsyncErrors(async (req,res,next)=>{
     })
 })
 
+//Get user details  => /api/v1/admin/user/:id
+exports.getUserDetails = catchAsyncErrors(async (req,res,next)=>{
+
+    const user = await User.findById(req.params.id);
+    if(!user){
+        return next(new ErrorHandler('User not found', 400))    
+    }
+    res.status(200).json({
+        success:true,
+        User:user
+    })
+
+})
+
 
