6985616a0ce01adf530ba92dd89debfaad3da847
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-03-14 10:05:11 +0200

    updated with order route

--- back_end/app.js
+++ back_end/app.js
@@ -4,17 +4,21 @@ const cookieParser = require('cookie-parser');
 
 const errorMiddlewares = require('./middlewares/errors');
 
+
 app.use(express.json());
 app.use(cookieParser());
+
 
 //Importing all the routes
 const products = require('./routes/products');
 const users = require('./routes/userAuth');
+const orders = require('./routes/orders');
+
 
 // doing routes
 app.use('/api/v1', products);
 app.use('/api/v1', users);
-
+app.use('/api/v1', orders);
 //Implementing the middleware to handle errors
 app.use(errorMiddlewares);
 
