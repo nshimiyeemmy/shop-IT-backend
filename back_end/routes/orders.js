095ac220b67a5e7f054c84b01df83e9de5c75dd0
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-03-14 10:14:03 +0200

    updated with get single order route

--- back_end/routes/orders.js
+++ back_end/routes/orders.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { newOrder } = require('../controllers/orderController');
+const { newOrder,getSingleOrder,myOrders } = require('../controllers/orderController');
 const {isUserAuthenticated} = require('../middlewares/authenticate');
 
 router.route('/order/new').post(isUserAuthenticated,newOrder);
+router.route('/order/:id').get(isUserAuthenticated,getSingleOrder);
+
 
 module.exports = router
