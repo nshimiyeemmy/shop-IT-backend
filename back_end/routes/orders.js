024253025325aeccc2b1617279bdfb4d318d67dc
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-03-14 10:19:19 +0200

    updated with get all orders from database route

--- back_end/routes/orders.js
+++ back_end/routes/orders.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { newOrder,getSingleOrder,myOrders } = require('../controllers/orderController');
+const { newOrder,allOrders,getSingleOrder,myOrders } = require('../controllers/orderController');
 const {isUserAuthenticated} = require('../middlewares/authenticate');
 
 router.route('/order/new').post(isUserAuthenticated,newOrder);
+router.route('/orders').get(isUserAuthenticated,allOrders);
 router.route('/order/:id').get(isUserAuthenticated,getSingleOrder);
 router.route('/orders/me').get(isUserAuthenticated,myOrders);
 
