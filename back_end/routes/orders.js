a513480c43a2c2cc7e947e9b154a28095cb9a60d
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-03-14 20:39:59 +0200

    updated

--- back_end/routes/orders.js
+++ back_end/routes/orders.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const { newOrder,allOrders,getSingleOrder,myOrders } = require('../controllers/orderController');
+const { newOrder,allOrders,getSingleOrder,myOrders,updateOrder} = require('../controllers/orderController');
 const {isUserAuthenticated,authorizeRole} = require('../middlewares/authenticate');
 
 router.route('/order/new').post(isUserAuthenticated,newOrder);
 router.route('/admin/orders').get(isUserAuthenticated,authorizeRole('admin'),allOrders);
-router.route('/admin/order/:id').get(isUserAuthenticated,authorizeRole('admin'),getSingleOrder);
 router.route('/orders/me').get(isUserAuthenticated,myOrders);
+router.route('/admin/order/:id').get(isUserAuthenticated,authorizeRole('admin'),getSingleOrder)
+router.route('/admin/order/:id').put(isUserAuthenticated,authorizeRole('admin'),updateOrder)
 
 module.exports = router
