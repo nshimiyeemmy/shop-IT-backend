20cc1d79d71db76cf677b65e0a009b294610ad8d
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-03-14 11:18:17 +0200

    updated wit authorize admin role

--- back_end/routes/orders.js
+++ back_end/routes/orders.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const { newOrder,allOrders,getSingleOrder,myOrders } = require('../controllers/orderController');
-const {isUserAuthenticated} = require('../middlewares/authenticate');
+const {isUserAuthenticated,authorizeRole} = require('../middlewares/authenticate');
 
 router.route('/order/new').post(isUserAuthenticated,newOrder);
-router.route('/admin/orders').get(isUserAuthenticated,allOrders);
-router.route('/admin/order/:id').get(isUserAuthenticated,getSingleOrder);
+router.route('/admin/orders').get(isUserAuthenticated,authorizeRole('admin'),allOrders);
+router.route('/admin/order/:id').get(isUserAuthenticated,authorizeRole('admin'),getSingleOrder);
 router.route('/orders/me').get(isUserAuthenticated,myOrders);
 
 module.exports = router
