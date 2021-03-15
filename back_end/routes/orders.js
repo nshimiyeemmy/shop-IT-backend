2989547da84f164ea045087399a9d8d17ee7d3e3
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-03-15 05:50:12 +0200

    updated with delete Order route

--- back_end/routes/orders.js
+++ back_end/routes/orders.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const { newOrder,allOrders,getSingleOrder,myOrders,updateOrder} = require('../controllers/orderController');
+const { newOrder,allOrders,getSingleOrder,myOrders,updateOrder,deleteOrder} = require('../controllers/orderController');
 const {isUserAuthenticated,authorizeRole} = require('../middlewares/authenticate');
 
 router.route('/order/new').post(isUserAuthenticated,newOrder);
 router.route('/admin/orders').get(isUserAuthenticated,authorizeRole('admin'),allOrders);
 router.route('/orders/me').get(isUserAuthenticated,myOrders);
 router.route('/admin/order/:id').get(isUserAuthenticated,authorizeRole('admin'),getSingleOrder)
-router.route('/admin/order/:id').put(isUserAuthenticated,authorizeRole('admin'),updateOrder)
+                                .put(isUserAuthenticated,authorizeRole('admin'),updateOrder)
+                                .delete(isUserAuthenticated,authorizeRole('admin'),deleteOrder);
+
 
 module.exports = router
