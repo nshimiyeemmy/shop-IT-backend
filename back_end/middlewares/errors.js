b1c139c8722b3a630d443bbec7c9af79e3d0d78e
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-04-30 11:41:14 +0200

    updated

--- back_end/middlewares/errors.js
+++ back_end/middlewares/errors.js
@@ -44,7 +44,7 @@ module.exports = (err,req,res,next)=>{
     res.status(error.statusCode).json({
         success:false,
         message: error.message || 'Internal Server Error'
-    }) 
+    })
    }
-   
+
 }
