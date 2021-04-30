b04e360af5d002b3a6dd40dd58d4ed67bd15a4ce
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-04-30 11:43:40 +0200

    updated

--- back_end/routes/products.js
+++ back_end/routes/products.js
@@ -8,12 +8,12 @@ const {isUserAuthenticated,authorizeRole} = require('../middlewares/authenticate
 
 router.route('/products').get(getProducts);
 router.route('/admin/products/new').post(isUserAuthenticated,authorizeRole('admin'),newProducts);
-router.route('/products/:id').get(getSingleProduct);
+router.route('/product/:id').get(getSingleProduct);
 router.route('/admin/products/:id').put(isUserAuthenticated,authorizeRole('admin'),updateProduct)
                                     .delete(isUserAuthenticated,authorizeRole('admin'),deleteProduct)
 
 router.route('/review/new').put(isUserAuthenticated,createProductReview);
 router.route('/admin/reviews').get(isUserAuthenticated,authorizeRole('admin'),getAllProductReviews);
 router.route('/review').delete(isUserAuthenticated,deleteProductReview);
-                          
+
 module.exports = router;
