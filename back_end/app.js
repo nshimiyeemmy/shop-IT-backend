ee295c3eeffea6de1e52cc6978c86bf4a6e46d00
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-05-01 14:20:11 +0200

    added cloudinary

--- back_end/app.js
+++ back_end/app.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const app = express();
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const cloudinary = require('cloudinary');
 
 const errorMiddlewares = require('./middlewares/errors');
 
 
+
 app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(cookieParser());
 
+//setting up our cloudinary config for uploading the images
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
 
 //Importing all the routes
 const products = require('./routes/products');
