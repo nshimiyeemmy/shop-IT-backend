0f4dbf42b9d145137fdc34c301a4aae70c2ef869
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-05-22 16:28:53 +0200

    updated

--- back_end/models/products.js
+++ back_end/models/products.js
@@ -1,52 +1,50 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const express = require('express');
 
-
 const productSchema = new mongoose.Schema({
-
-    name:{
-        type:String,
-        required:[true,'Please enter product name'],
-        trim:true,
-        maxLength:[200,'Product name cannot exceed 200 characters'],
+    name: {
+        type: String,
+        required: [true, 'Please enter product name'],
+        trim: true,
+        maxLength: [200, 'Product name cannot exceed 200 characters'],
     },
-    price:{
-        type:Number,
-        required:[true,'Please enter product Price'],
-        maxLength:[5, 'Product name cannot exceed 5 characters'],
-        default:0.0
+    price: {
+        type: Number,
+        required: [true, 'Please enter product Price'],
+        maxLength: [5, 'Product name cannot exceed 5 characters'],
+        default: 0.0,
     },
-    description:{
-        type:String,
-        required:[true,'Please enter product Description'],
+    description: {
+        type: String,
+        required: [true, 'Please enter product Description'],
     },
-    manufacturer:{
-        type:String,
-        required:[true,'Please enter Manufacturer name'],
-        maxLength:[20, 'Manufacturer name cannot exceed 20 characters'],
+    manufacturer: {
+        type: String,
+        required: [true, 'Please enter Manufacturer name'],
+        maxLength: [20, 'Manufacturer name cannot exceed 20 characters'],
     },
-    ratings:{
-        type:Number,
-        default:0
+    ratings: {
+        type: Number,
+        default: 0,
     },
-    images:[
+    images: [
         {
-            public_id:{
-                type:String,
-                required:true
+            public_id: {
+                type: String,
+                required: true,
             },
-            url:{
-                type:String,
-                required:true
-            }
-       }
+            url: {
+                type: String,
+                required: true,
+            },
+        },
     ],
-    category:{
-        type:String,
-        required:[true, 'Please select the category for this product'],
+    category: {
+        type: String,
+        required: [true, 'Please select the category for this product'],
         //I defined product Categories an enum values
-        enum:{
-            values:[
+        enum: {
+            values: [
                 'Electronics',
                 'Cameras',
                 'Laptops',
@@ -56,50 +54,50 @@ const productSchema = new mongoose.Schema({
                 'Education/Books',
                 'Beauty/Health',
                 'Sports',
-                'Outdoor'
+                'Outdoor',
             ],
-            message: 'Please select the correct category for Product'
-        }
+            message: 'Please select the correct category for Product',
+        },
     },
-    quantity:{
-        type:Number,
-        required:[true, 'Please Enter Product Quantity in Stock'],
-        maxLength:[5, 'Product Quantity can not exceed 5 characters'],
-        default:0
+    quantity: {
+        type: Number,
+        required: [true, 'Please Enter Product Quantity in Stock'],
+        maxLength: [5, 'Product Quantity can not exceed 5 characters'],
+        default: 0,
     },
-    numOfReviews:{
-        type:Number,
-        default:0
+    numOfReviews: {
+        type: Number,
+        default: 0,
     },
-    reviews:[
+    reviews: [
         {
-            user:{
-                type:mongoose.Schema.ObjectId,
-                ref:'User',
-                required:true
+            user: {
+                type: mongoose.Schema.ObjectId,
+                ref: 'User',
+                required: true,
             },
-            name:{
-                type:String,
-                required:true
+            name: {
+                type: String,
+                required: true,
             },
-            rating:{
-                type:Number,
-                required:true
+            rating: {
+                type: Number,
+                required: true,
             },
-            comment:{
-                type:String,
-                required:true
-            }
-        }
+            comment: {
+                type: String,
+                required: true,
+            },
+        },
     ],
     // user:{
     //     type:mongoose.Schema.ObjectId,
     //     ref:'User',
     //     required:true
     // },
-    createdAt:{
-        type:Date,
-        default:Date.now()
-        }
-})
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+});
 module.exports = mongoose.model('Products', productSchema);
