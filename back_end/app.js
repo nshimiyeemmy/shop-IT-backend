668eff7f39b01d2bd0271b136511768cb67a94f3
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-05-22 16:26:18 +0200

    added cloudinary into our app

--- back_end/app.js
+++ back_end/app.js
@@ -1,23 +1,15 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const cloudinary = require('cloudinary');
+const fileUpload = require('express-fileupload');
 
 const errorMiddlewares = require('./middlewares/errors');
 
-
-
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-//setting up our cloudinary config for uploading the images
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-})
+app.use(fileUpload());
 
 //Importing all the routes
 const products = require('./routes/products');
@@ -32,4 +24,4 @@ app.use('/api/v1', orders);
 //Implementing the middleware to handle errors
 app.use(errorMiddlewares);
 
-module.exports = app
+module.exports = app;
