62a356fef8535cc814936f4718286280d1488b64
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-05-22 16:28:06 +0200

    updated

--- back_end/middlewares/errors.js
+++ back_end/middlewares/errors.js
@@ -28,7 +28,6 @@ module.exports = (err,req,res,next)=>{
           const message = Object.values(err.errors).map(value =>value.message);
           error = new ErrorHandler(message,400);
       }
-
       //Handling the Mongoose Duplicates error
       if(err.code === 11000){
           const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
