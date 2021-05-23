554e54e32543d7929b76231bf072c0eb5d035362
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-05-23 17:10:06 +0200

    added way to send email

--- back_end/utils/sendEmail.js
+++ back_end/utils/sendEmail.js
@@ -1,20 +1,19 @@
 const nodemailer = require('nodemailer');
-const sendEmail = async options => {
-
-    const  transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
-        auth: {
-          user: process.env.SMTP_EMAIL,
-          pass: process.env.SMTP_PASSWORD
-        }
-      });
-      const message = {
-          from:`${process.env.SMTP_SENT_FROM_NAME}<${process.env.SMTP_SENT_FROM_EMAIL}>`,
-          to: options.email,
-          subject:options.subject,
-          text:options.message
-      }
-await transporter.sendMail(message);
-}
-module.exports = sendEmail
+const sendEmail = async (options) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_EMAIL,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
+  const message = {
+    from: `${process.env.SMTP_SENT_FROM_NAME}<${process.env.SMTP_SENT_FROM_EMAIL}>`,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+  await transporter.sendMail(message);
+};
+module.exports = sendEmail;
