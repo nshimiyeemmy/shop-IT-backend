29654b45dd63ee13a8d43a213aa1aeb5c367fe4b
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-05-23 17:09:40 +0200

    added way to send token in the cookie

--- back_end/utils/JWT.js
+++ back_end/utils/JWT.js
@@ -1,21 +1,21 @@
 let tokens = require('../models/tokens');
 //create, send and save the JWT token in the cookie
-const sendToken = (user, statusCode,res)=>{
-    //create JWT token
-    const token = user.getJwtToken();
-    //create Options for the cookie
-    const options = {
-        expires: new Date(
-            Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
-        ),
-        httpOnly:true
-    }
-    tokens.create({token:token});
+const sendToken = (user, statusCode, res) => {
+  //create JWT token
+  const token = user.getJwtToken();
+  //create Options for the cookie
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  tokens.create({ token: token });
 
-    res.status(statusCode).cookie('token',token,options).json({
-        success:true,
-        token:token,
-        User:user
-    })
-}
-module.exports = sendToken
+  res.status(statusCode).cookie('token', token, options).json({
+    success: true,
+    token,
+    user,
+  });
+};
+module.exports = sendToken;
