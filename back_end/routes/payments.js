8fd51a81692ffc45e26d359f1ea1c276bad59e7f
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-06-12 15:50:05 +0200

    renamed to payment

--- back_end/routes/payments.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { processPayment } = require('../controllers/paymentController');
-const { isUserAuthenticated } = require('../middlewares/authenticate');
-
-router.route('/payment/process').post(isUserAuthenticated, processPayment);
-module.exports = router;
