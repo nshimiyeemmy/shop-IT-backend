896140838d7bf262657c1f0954c75cf27c241216
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-06-12 11:15:53 +0200

    updated

--- back_end/routes/products.js
+++ back_end/routes/products.js
@@ -1,19 +1,35 @@
 const express = require('express');
 const router = express.Router();
 
-
-const { getSingleProduct,newProducts, getProducts,updateProduct,deleteProduct,createProductReview,getAllProductReviews
-    ,deleteProductReview} = require('../controllers/productController');
-const {isUserAuthenticated,authorizeRole} = require('../middlewares/authenticate');
+const {
+  getSingleProduct,
+  newProducts,
+  getProducts,
+  updateProduct,
+  deleteProduct,
+  createProductReview,
+  getAllProductReviews,
+  deleteProductReview,
+} = require('../controllers/productController');
+const {
+  isUserAuthenticated,
+  authorizeRole,
+} = require('../middlewares/authenticate');
 
 router.route('/products').get(getProducts);
-router.route('/admin/products/new').post(isUserAuthenticated,authorizeRole('admin'),newProducts);
+router
+  .route('/admin/products/new')
+  .post(isUserAuthenticated, authorizeRole('admin'), newProducts);
 router.route('/product/:id').get(getSingleProduct);
-router.route('/admin/products/:id').put(isUserAuthenticated,authorizeRole('admin'),updateProduct)
-                                    .delete(isUserAuthenticated,authorizeRole('admin'),deleteProduct)
+router
+  .route('/admin/products/:id')
+  .put(isUserAuthenticated, authorizeRole('admin'), updateProduct)
+  .delete(isUserAuthenticated, authorizeRole('admin'), deleteProduct);
 
-router.route('/review/new').put(isUserAuthenticated,createProductReview);
-router.route('/admin/reviews').get(isUserAuthenticated,authorizeRole('admin'),getAllProductReviews);
-router.route('/review').delete(isUserAuthenticated,deleteProductReview);
+router.route('/review/new').put(isUserAuthenticated, createProductReview);
+router
+  .route('/admin/reviews')
+  .get(isUserAuthenticated, authorizeRole('admin'), getAllProductReviews);
+router.route('/review').delete(isUserAuthenticated, deleteProductReview);
 
 module.exports = router;
