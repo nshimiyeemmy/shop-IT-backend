1ad2d6e1ede5e16e9420a0edc83d8223b11538da
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-06-12 15:49:44 +0200

    updated to remove errors encountered during payment

--- back_end/controllers/paymentController.js
+++ back_end/controllers/paymentController.js
@@ -1,10 +1,10 @@
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+require('dotenv').config({ path: 'back_end/config/config.env' });
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 //PROCESSING THE STRIPE PAYMENTS => /api/v1/payment/process
-
 exports.processPayment = catchAsyncErrors(async (req, res, next) => {
-  const payment = await stripe.paymentIntent.create({
+  const paymentIntent = await stripe.paymentIntents.create({
     amount: req.body.amount,
     currency: 'usd',
 
@@ -12,7 +12,13 @@ exports.processPayment = catchAsyncErrors(async (req, res, next) => {
   });
   res.status(200).json({
     success: true,
-    message: 'Payment was successfully',
-    client_Secret: paymentIntent.client_Secret,
+    client_secret: paymentIntent.client_secret,
+  });
+});
+
+//Send Stripe API KEY to the front end => /api/v1/stripeapi
+exports.sendStripeApi = catchAsyncErrors(async (req, res, next) => {
+  res.status(200).json({
+    StripeApiKey: process.env.STRIPE_API_KEY,
   });
 });
